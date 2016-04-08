69eee6828cf4a95e4ef4e0fe2955c0f92fa009e8
zuweizhao <zuweizhao@outlook.com>
2016-04-08 20:49:41 +0800

    增加了ResponseBody，修改了相关请求接口。

--- AQI_NJU/src/main/java/edu/nju/aqi/controller/AirQualityController.java
+++ AQI_NJU/src/main/java/edu/nju/aqi/controller/AirQualityController.java
@@ -1,13 +1,16 @@
 package edu.nju.aqi.controller;
 
 import edu.nju.aqi.bo.AirQualityBo;
+import edu.nju.aqi.model.AirQuality;
 import edu.nju.aqi.service.AirQualityService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
 import java.util.List;
 
 @Controller
@@ -21,32 +24,45 @@ public class AirQualityController {
     @RequestMapping("/getAllAirQuality")
     public String getAllAirQuality() {
         //request.setAttribute("airQualityList", airQualityService.getAllAirQuality());
-        System.out.println(airQualityService.getAllAirQuality().get(0).getId());
+        //System.out.println(airQualityService.getAllAirQuality().get(0).getId());
         return "/login";
     }
 
+    @ResponseBody
     @RequestMapping("/getTodaysAirQuality")
-    public String getTodaysAirQuality(String city, HttpServletRequest request) {
+    public List<AirQuality> getTodaysAirQuality(String city, HttpServletRequest request) {
         request.setAttribute("airQualityList", airQualityService.getTodaysAirQuality(city));
-        System.out.println(airQualityService.getTodaysAirQuality(city).size());
-        return "/login";
+        //System.out.println(airQualityService.getTodaysAirQuality(city).size());
+        return airQualityService.getTodaysAirQuality(city);
     }
 
+    @ResponseBody
     @RequestMapping("/getAllTodaysAirQuality")
-    public String getAllTodaysAirQuality(HttpServletRequest request) {
+    public List<AirQuality> getAllTodaysAirQuality(HttpServletRequest request) {
         request.setAttribute("airQualityList", airQualityService.getAllTodaysAirQuality());
-        System.out.println(airQualityService.getAllTodaysAirQuality().size());
-        return "/login";
+        //System.out.println(airQualityService.getAllTodaysAirQuality().size());
+        return airQualityService.getAllTodaysAirQuality();
     }
 
+    @ResponseBody
     @RequestMapping("/getCurrentAirQuality")
-    public String getCurrentAirQuality(String city) {
-        return "/city";
+    public AirQuality getCurrentAirQuality(String city, HttpServletRequest request) {
+    	request.setAttribute("airQualityList", airQualityService.getCurrentAirQuality(city));
+        //System.out.println(airQualityService.getTodaysAirQuality(city).size());
+        return airQualityService.getCurrentAirQuality(city);
     }
 
     @ResponseBody
     @RequestMapping("/getAllCurrentAirQuality")
     public List<AirQualityBo> getAllCurrentAirQuality() {
+    	//System.out.println(airQualityService.getAllCurrentAirQuality().size());
         return airQualityService.getAllCurrentAirQuality();
+    }
+    
+    @ResponseBody
+    @RequestMapping("/get24HoursAirQuality")
+    public List<AirQuality> get24HoursAirQuality(String city, HttpServletRequest request) {
+    	//System.out.println(airQualityService.get24HoursAirQuality(city));
+        return airQualityService.get24HoursAirQuality(city);
     }
 }
